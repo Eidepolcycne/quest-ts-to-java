ffc41b6001c29e455bb26078aeab6d3ffb4670cc
Eidepolcycne <fbab1@free.fr>
2023-03-02 11:27:17 +0100

    changed

--- User.java
+++ User.java
@@ -6,7 +6,7 @@ public class User {
   public static void main(String[] args) {
     Date birthdate;
     double size;
-    ArrayList <String> photos = new ArrayList<String>();
+    ArrayList <Photo> photos;
     Address address;
   }
 
